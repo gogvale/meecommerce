db1041c39601aabd3c1c1c96968dcb10550d9305
eduardoleal <eduardo.leal.or@gmail.com>
2022-02-05 12:33:32 -0600

    feat: added recovery screen and added functionality

--- app/src/main/java/dev/eduardoleal/meecommerce/auth/Recovery.java
+++ app/src/main/java/dev/eduardoleal/meecommerce/auth/Recovery.java
@@ -1,16 +1,58 @@
 package dev.eduardoleal.meecommerce.auth;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import dev.eduardoleal.meecommerce.R;
 
 public class Recovery extends AppCompatActivity {
 
+    EditText edtRecoveryPassword;
+    Button btnRecoveryPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recovery);
+
+        edtRecoveryPassword = findViewById(R.id.edt_email_recovery);
+        btnRecoveryPassword = findViewById(R.id.btn_password_recovery);
+
+        btnRecoveryPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtRecoveryPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Invalid email", Toast.LENGTH_SHORT).show();
+                } else {
+                    onRecoverPassword(email);
+                }
+            }
+        });
+    }
+
+    private void onRecoverPassword(String email) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "The email was sent, check your email.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "There is an error, please try again later", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
