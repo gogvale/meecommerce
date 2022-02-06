25423aaf300c88b04ec9133a219d50c1ba943c2c
eduardoleal <eduardo.leal.or@gmail.com>
2022-02-06 01:39:26 -0600

    feat: added scroll in profile and added new library of read images from url, set new text of cheers periods of day

--- app/src/main/java/dev/eduardoleal/meecommerce/Profile.java
+++ app/src/main/java/dev/eduardoleal/meecommerce/Profile.java
@@ -1,24 +1,32 @@
 package dev.eduardoleal.meecommerce;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.util.Calendar;
+
 public class Profile extends AppCompatActivity {
 
+    TextView txtCheers;
     EditText edtFullName, edtEmail, edtPhone;
     Button btnUpdate;
+    ImageView imageProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,8 @@ public class Profile extends AppCompatActivity {
         edtEmail = findViewById(R.id.edt_email_profile);
         edtPhone = findViewById(R.id.edt_phone_profile);
         btnUpdate = findViewById(R.id.btn_update_profile);
+        imageProfile = findViewById(R.id.imageProfile);
+        txtCheers = findViewById(R.id.txt_cheers);
 
         onUpdateUI();
 
@@ -68,10 +78,28 @@ public class Profile extends AppCompatActivity {
             String name = user.getDisplayName();
             String email = user.getEmail();
             String phone = user.getPhoneNumber();
+            Uri image = user.getPhotoUrl();
 
             edtFullName.setText(name);
             edtEmail.setText(email);
             edtPhone.setText(phone);
+            Glide.with(getApplicationContext()).load(image).into(imageProfile);
+
+            onSetCheers();
+        }
+    }
+
+    private void onSetCheers(){
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            txtCheers.setText("Good Morning");
+        }else if(timeOfDay >= 12 && timeOfDay < 16){
+            txtCheers.setText("Good Afternoon");
+        }else if(timeOfDay >= 16 && timeOfDay < 21){
+            txtCheers.setText("Good Evening");
+        }else if(timeOfDay >= 21 && timeOfDay < 24){
+            txtCheers.setText("Good Night");
         }
     }
 }
